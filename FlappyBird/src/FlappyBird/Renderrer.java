0fc105b41be0f0b45f3a477186c3d49fbe9c4988
DaV71 <dsop13571@gmail.com>
2019-01-23 22:08:48 +0100

    Final Version

---
+++ FlappyBird/src/FlappyBird/Renderrer.java
@@ -0,0 +1,14 @@
+package FlappyBird;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+public class Renderrer extends JPanel {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
